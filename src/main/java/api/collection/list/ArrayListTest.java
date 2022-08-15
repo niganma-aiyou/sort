91cd68e990cfa2b1d514adad9d4bc053ad882903
chuweiyuan <123@456.com>
2022-08-15 08:33:15 +0800

    基础知识学习

--- src/main/java/api/collection/list/ArrayListTest.java
+++ src/main/java/api/collection/list/ArrayListTest.java
@@ -6,6 +6,9 @@ package api.collection.list;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * TODO
  * TODO
@@ -19,6 +22,40 @@ public class ArrayListTest {
     private static final Logger log = LogManager.getLogger(ArrayListTest.class.getName());
 
     public static void main(String[] args) {
-        log.info("");
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Tom");
+        list.add("Jerry");
+
+        int length = list.size();
+        log.info(length);
+
+        String s = list.get(0);
+        log.info(s);
+
+        //add(int index, Object element)  方法用于在集合指定位置添加元素，原集合中从指定位置开始的元素全部后置
+        list.add(0,"MMM");
+        log.info(list.get(0));
+        log.info(list.get(1));
+
+        list.set(0,"TTT");
+        log.info(list.get(0));
+
+        /*Iterator<String> iterator = list.iterator();
+        while(iterator.hasNext()){
+            log.info(iterator.next());
+        }*/
+        for (String ss : list
+             ) {
+            log.info(ss);
+        }
+
+        log.info(list.contains("Tom"));
+
+        log.info(list.remove(0));
+
+        log.info(list.isEmpty());
+        list.clear();
+        log.info(list.isEmpty());
+
     }
 }
--- src/main/java/api/collection/list/LinkedListTest.java
+++ src/main/java/api/collection/list/LinkedListTest.java
@@ -6,6 +6,8 @@ package api.collection.list;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.LinkedList;
+
 /**
  * TODO
  * TODO
@@ -19,6 +21,34 @@ public class LinkedListTest {
     private static final Logger log = LogManager.getLogger(LinkedListTest.class.getName());
 
     public static void main(String[] args) {
-        log.info("");
+        
+        LinkedList<String> s = new LinkedList<>();
+
+        s.add("语文");
+        s.add("数学");
+        s.add("英语");
+
+//        addFirst(E e) ：在此列表的开始处插入指定的元素。
+//        addLast(E e) ：将指定的元素列表的结束。
+        s.addFirst("历史");
+        s.addLast("地理");
+
+//        getFirst() ：返回此列表中的第一个元素。
+//        getLast() ：返回此列表中的最后一个元素。
+        log.info(s.getFirst());
+        log.info(s.getLast());
+
+//        removeFirst() ：移除并返回此列表中的最后一个元素。
+//        removeLast() ：移除并返回此列表中的最后一个元素。
+        s.removeFirst();
+        s.removeLast();
+
+
+        log.info(s);
+
+        for(String ss : s){
+            log.info(ss);
+        }
+
     }
 }
